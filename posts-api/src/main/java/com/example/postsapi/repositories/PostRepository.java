dfd5ec719f6fcbafd77530ca0c86dd06bc2c6e2b
severejetlag <nhl1013@gmail.com>
2018-04-11 15:09:43 -0700

    Created Post Repository to handle DB connectios for posts api

---
+++ posts-api/src/main/java/com/example/postsapi/repositories/PostRepository.java
@@ -0,0 +1,8 @@
+package com.example.postsapi.repositories;
+
+import com.example.postsapi.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long>{
+
+}
