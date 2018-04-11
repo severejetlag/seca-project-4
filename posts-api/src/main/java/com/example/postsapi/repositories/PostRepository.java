95f6362dcb064a384a2fdf4f6016ca2bed4fb5fc
severejetlag <nhl1013@gmail.com>
2018-04-11 16:12:10 -0700

    Updating PostRepository to return a List when completeing findAll()

--- posts-api/src/main/java/com/example/postsapi/repositories/PostRepository.java
+++ posts-api/src/main/java/com/example/postsapi/repositories/PostRepository.java
@@ -3,6 +3,9 @@ package com.example.postsapi.repositories;
 import com.example.postsapi.models.Post;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PostRepository extends CrudRepository<Post, Long>{
+    List<Post> findAll();
 
 }
