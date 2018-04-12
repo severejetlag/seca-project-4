54837c7d5c8bf259d5714ff35e390645910e169b
severejetlag <nhl1013@gmail.com>
2018-04-11 20:56:52 -0700

    Removed old migration files that were in the users api. Also removed flyway from the build.gradle file so it would not error out or have any chance of making migrations.

--- users-api/src/main/resources/db/migration/V1__Create_users_table.sql
+++
@@ -1,9 +0,0 @@
-create table USERS (
-    ID serial,
-    USER_NAME varchar(100) NOT NULL,
-    FIRST_NAME varchar(100) NOT NULL,
-    LAST_NAME varchar(100) NOT NULL,
-    PASSWORD varchar(100) NOT NULL,
-    NEIGHBORHOOD varchar(100) NOT NULL,
-    BIO varchar NOT NULL
-);
--- users-api/src/main/resources/db/migration/V2__Create_posts_table.sql
+++
@@ -1,9 +0,0 @@
-create table POSTS (
-    ID serial,
-    TITLE varchar NOT NULL,
-    FIRST_NAME varchar(100),
-    LAST_NAME varchar(100),
-    POST_BODY varchar NOT NULL,
-    DATE_CREATED timestamptz NOT NULL DEFAULT now(),
-    APPROVED boolean NOT NULL DEFAULT false
-);
