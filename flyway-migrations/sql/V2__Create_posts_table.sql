fece9519f0ebbd2bfb8862beae4c3787907b1242
severejetlag <nhl1013@gmail.com>
2018-04-12 13:31:41 -0700

    Updated table to use timestamp lunch. Added post data to migration file. As well as updated docker-compose.yml

--- flyway-migrations/sql/V2__Create_posts_table.sql
+++ flyway-migrations/sql/V2__Create_posts_table.sql
@@ -5,7 +5,7 @@ create table POSTS (
     LAST_NAME varchar(100),
     CONTACT_DETAILS varchar,
     POST_BODY varchar NOT NULL,
-    DATE_CREATED timestamptz NOT NULL DEFAULT now(),
+    DATE_CREATED timestamp NOT NULL DEFAULT NOW(),
     APPROVED boolean NOT NULL DEFAULT false,
     VERIFIED boolean NOT NULL DEFAULT false
 );
