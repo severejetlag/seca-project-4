8173ae47f97746e235bb50ffa1a6b7227241149c
severejetlag <nhl1013@gmail.com>
2018-04-13 00:30:01 -0700

    Added admin page link that will only appear for admin user.

--- ui/src/components/PostList.js
+++ ui/src/components/PostList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Post from './Post'
+import PostNewForm from './PostNewForm'
 
 const PostList = props => {
   const PostComponents = props.posts.map((post, index) => {
@@ -14,6 +15,13 @@ const PostList = props => {
   })
   return(
     <ul className='list-group list-group-flush' id='post-list'>
+      <li className='post list-group-item'>
+        <PostNewForm
+          currentUser={props.currentUser}
+          hasCurrentUser={props.hasCurrentUser}
+          createPost={props.createPost}
+        />
+      </li>
       {PostComponents}
     </ul>
   )
