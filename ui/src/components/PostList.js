7036b95f5b85d386de3003868504db1fadd1f7df
severejetlag <nhl1013@gmail.com>
2018-04-12 21:55:15 -0700

    Add map through posts to create PostCompoments to be displayed.

--- ui/src/components/PostList.js
+++ ui/src/components/PostList.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Post from './Post'
 
 const PostList = props => {
+  const PostComponents = props.posts.map((post, index) => {
+    return (
+      <Post
+        adminUser={props.adminUser}
+        post={post}
+        key={index}
+        index={index}
+      />
+    )
+  })
   return(
     <ul>
-      Post list
+      {PostComponents}
     </ul>
   )
 }
